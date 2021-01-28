d0282da14908942e67d4bb4d852a2b1496bfd612
sergioluizsoares <sl.soares@hotmail.com>
2021-01-28 00:22:09 -0300

    first commit

---
+++ src/containers/Main/components/Card.jsx
@@ -0,0 +1,22 @@
+import React, { memo } from 'react'
+import ProtoTypes from 'prop-types'
+import { Card as CardUI } from '../../../components'
+import { 
+  LabelStyled,
+  ValueStyled,
+  CardContentStyled
+} from './style'
+import { CardContent } from '../../../components/Card'
+
+function Card({ value, label, color }) {
+  return (
+    <CardUI>
+      <CardContentStyled color={color}>
+        <ValueStyled>{value}</ValueStyled> 
+        <LabelStyled>{label}</LabelStyled>
+      </CardContentStyled>
+    </CardUI>
+  )
+}
+
+export default memo(Card)
